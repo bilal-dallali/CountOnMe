1ffd47875f0df599a80c8db20d1c6a5834e3e069
bilal-dallali <dallali-bilal@hotmail.fr>
2024-02-05 15:24:18 +0100

    test multiplication substraction division

--- CountOnMeTests/SimpleCalcTests.swift
+++ CountOnMeTests/SimpleCalcTests.swift
@@ -20,6 +20,37 @@ class SimpleCalcTests: XCTestCase {
         let result = try calculator.calculate()
         XCTAssertEqual(result, "12")
     }
+    
+    func testSuccessfulSubstraction() throws {
+        //let baseNumber = 4
+        let calculator = Calculator()
+        calculator.appendElement("16")
+        try calculator.addOperand(.minus)
+        calculator.appendElement("6")
+        let result = try calculator.calculate()
+        XCTAssertEqual(result, "10")
+    }
+    
+    func testSuccessfulMultiplication() throws {
+        //let baseNumber = 4
+        let calculator = Calculator()
+        calculator.appendElement("4")
+        try calculator.addOperand(.multiply)
+        calculator.appendElement("8")
+        let result = try calculator.calculate()
+        XCTAssertEqual(result, "32")
+    }
+    
+    func testSuccessfulDivision() throws {
+        //let baseNumber = 4
+        let calculator = Calculator()
+        calculator.appendElement("100")
+        try calculator.addOperand(.plus)
+        calculator.appendElement("4")
+        let result = try calculator.calculate()
+        XCTAssertEqual(result, "25")
+    }
+
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
